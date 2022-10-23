a9017c7c1d39f97e143e9b486e57929a015620c4
Eldar Faizov <e.faizov@qiwi.com>
2022-10-23 17:15:24 +0300

    [inc8] remove template file

--- internal/handlers/info.go
+++ internal/handlers/info.go
@@ -7,8 +7,12 @@ import (
 
 var tmpl *template.Template
 
+const templateString = `<ul>
+    {{range .}}{{if eq .MType "gauge"}}<li>{{.ID}} - {{.Value}}</li>{{else}}<li>{{.ID}} - {{.Delta}}</li>{{end}}{{end}}
+</ul>`
+
 func init() {
-	tmpl = template.Must(template.ParseFiles("layout.html"))
+	tmpl = template.Must(template.New("info").Parse(templateString))
 }
 func (m *MetricsHandlers) Info(w http.ResponseWriter, r *http.Request) {
 	data := m.Store.GetAll()
